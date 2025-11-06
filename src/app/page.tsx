13521d8c1ac57009283bdfc3425aa7ca8259fe73
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-11-06 13:16:02 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Hero from '@/components/Hero';
-import FeaturedTours from '@/components/FeaturedTours';
-import Newsletter from '@/components/Newsletter';
-
 export default async function Home() {
   return (
     <main className="flex flex-col">
-      <Hero />
-      <FeaturedTours />
-      <Newsletter />
+      <h1>Hello World</h1>
     </main>
   );
 }
