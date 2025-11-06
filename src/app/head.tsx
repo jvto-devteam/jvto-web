b602be8a6a306754d7d2b1ede2ace3b6f2d7d2c6
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-11-06 13:23:23 +0700

    update

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      {/* Gunakan link font di sini agar tidak muncul warning no-page-custom-font */}
+      <link
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@200..700"
+        rel="stylesheet"
+      />
+    </>
+  );
+}
