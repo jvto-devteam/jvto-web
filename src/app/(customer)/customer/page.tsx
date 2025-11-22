b9ea1e702a97b2a5841bcdf63b4f73b4971eb6cf
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-11-22 14:59:58 +0700

    update

---
+++ src/app/(customer)/customer/page.tsx
@@ -0,0 +1,8 @@
+export default function CustomerPage() {
+  return (
+    <div>
+      <h1>Customer Page</h1>
+      <p>Welcome to the customer section of the site.</p>
+    </div>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,42 +1,57 @@
 // middleware.ts
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
+  const url = req.nextUrl
+  const rawHost = req.headers.get("host") || "";
+  const host = rawHost.split(":")[0];
+  const domain = "java-tour";
+
+  
+  if (host === `my.${domain}.local` || host === `my.${domain}.com`) {
+    if (url.pathname === '/') {
+      url.pathname = '/customer'
+      return NextResponse.rewrite(url)
+    }
+    return NextResponse.next()
+  }
+
+
 
   // daftar exact URL delete permanent
   const goneUrls = [
-    '/student-package',
-    '/packages/activity/east-java-snapshot',
-    '/packages/activity/3D2N-java-volcano-tour-packages',
-    '/packages/activity/essentials-of-east-java-from-bali',
-    '/packages/activity/best-of-east-java-from-surabaya',
-    '/packages/activity/best-of-east-java-from-bali',
-    '/packages/activity/highlights-of-east-java-from-surabaya',
-    '/packages/activity/premiere-of-east-java',
-    '/packages/surabaya/3d2n/3',
-    '/packages/surabaya/4d3n/4',
-    '/packages/surabaya/4d3n/5',
-    '/packages/surabaya/4d3n/6',
-    '/packages/surabaya/5d4n/4',
-    '/packages/surabaya/5d4n/9',
-    '/packages/surabaya/6d5n/2',
-    '/packages/bali/3d2n/3',
-    '/packages/bali/4d3n/3',
-    '/packages/bali/4d3n/4',
-    '/accommodations',
-    '/transportations',
+    "/student-package",
+    "/packages/activity/east-java-snapshot",
+    "/packages/activity/3D2N-java-volcano-tour-packages",
+    "/packages/activity/essentials-of-east-java-from-bali",
+    "/packages/activity/best-of-east-java-from-surabaya",
+    "/packages/activity/best-of-east-java-from-bali",
+    "/packages/activity/highlights-of-east-java-from-surabaya",
+    "/packages/activity/premiere-of-east-java",
+    "/packages/surabaya/3d2n/3",
+    "/packages/surabaya/4d3n/4",
+    "/packages/surabaya/4d3n/5",
+    "/packages/surabaya/4d3n/6",
+    "/packages/surabaya/5d4n/4",
+    "/packages/surabaya/5d4n/9",
+    "/packages/surabaya/6d5n/2",
+    "/packages/bali/3d2n/3",
+    "/packages/bali/4d3n/3",
+    "/packages/bali/4d3n/4",
+    "/accommodations",
+    "/transportations",
   ];
 
   // 410 untuk exact match
   if (goneUrls.includes(pathname)) {
-    return new NextResponse('410 Gone', { status: 410 });
+    return new NextResponse("410 Gone", { status: 410 });
   }
 
   // 410 untuk semua URL dengan prefix /packages/yogyakarta
-  if (pathname.startsWith('/packages/yogyakarta')) {
-    return new NextResponse('410 Gone', { status: 410 });
+  if (pathname.startsWith("/packages/yogyakarta")) {
+    return new NextResponse("410 Gone", { status: 410 });
   }
 
   return NextResponse.next();
