f6005e404c4e19772238d61e790a881ff4ec4a5a
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-12-09 15:40:22 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
+  eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    images: {
     remotePatterns: [
       {
         protocol: 'https',
