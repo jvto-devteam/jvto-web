616106daee46118963ed53a6376495923ea47de6
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-12-12 17:00:32 +0700

    updaye

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -58,9 +58,9 @@ export default function WebsiteLayout({
     <>
       <div className="bg-background-light dark:bg-background-dark font-display text-ink-neutral-700 dark:text-ink-neutral-300">
         {/* GA khusus segment (website) */}
-        <Suspense>
+        {/* <Suspense>
           {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && <GoogleAnalytics />}
-        </Suspense>
+        </Suspense> */}
 
         <div className="min-h-screen flex flex-col bg-white">
           {/* Header / Navbar website */}
