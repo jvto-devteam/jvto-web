fd99737f3ee2ace19751c022b349c2bacd49ea20
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-12-12 14:24:15 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'javavolcano-touroperator.com',
+        hostname: 'legacy.javavolcano-touroperator.com',
         port: '',
         pathname: '/**',
       },
