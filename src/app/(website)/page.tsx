8653e937bfec54657555680c2b4f26a8d9e94f73
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2026-01-10 11:57:21 +0700

    Update page.tsx

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -143,6 +143,17 @@ const Home = async () => {
           "https://www.indecon.id/spotlight-networks/java-volcano-tour-operator",
         ],
       },
+      {
+        "@type": "WebSite",
+        "@id": siteUrl + "/#website",
+        url: siteUrl,
+        name: "Java Volcano Tour Operator",
+        alternateName: "JVTO",
+        publisher: {
+          "@id": siteUrl + "/#organization",
+        },
+        inLanguage: "en",
+      },
 
     ],
   };
