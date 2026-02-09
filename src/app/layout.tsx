9c932fb9ff7477263adbd89163daffe99da450b7
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2026-02-09 11:16:02 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script"; // Import Script dari Next.js
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,9 +14,41 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <head>
+        {/* Google Tag Manager - Script (Head) */}
+        {gtmId && (
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}');`,
+            }}
+          />
+        )}
+      </head>
+      <body className={`${inter.className} antialiased`}>
+        {/* Google Tag Manager (noscript) - Script (Body) */}
+        {gtmId && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
+
+        {children}
+      </body>
     </html>
   );
 }
