df980e38b2cbca8009aeba809f6faf5e2d5ad13a
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2026-03-14 12:46:14 +0700

    Update layout.tsx

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -57,13 +57,9 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1, // Ini yang akan membuka gembok ekstraksi AI!
-    },
+    "max-video-preview": -1,
+    "max-image-preview": "large",
+    "max-snippet": -1,
   },
 };
 
