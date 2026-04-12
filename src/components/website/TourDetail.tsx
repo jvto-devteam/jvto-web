3dbf4bc1600c6a11356b5e1928a7b60892c48f6b
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2026-04-12 16:26:58 +0700

    Update TourDetail.tsx

--- src/components/website/TourDetail.tsx
+++ src/components/website/TourDetail.tsx
@@ -306,7 +306,8 @@ export default function PackageDetailPage({ initialData,reviews }: Props) {
     { start: "2026-03-16", end: "2026-03-20" },
     { start: "2026-04-05", end: "2026-04-11" },
     { start: "2026-04-30", end: "2026-05-01" },
-    { start: "2026-05-30", end: "2026-06-02" },
+    { start: "2026-05-30", end: "2026-05-30" },
+    { start: "2026-06-02", end: "2026-06-02" },
   ];
 
   const isDateBlocked = (dateStr) => {
