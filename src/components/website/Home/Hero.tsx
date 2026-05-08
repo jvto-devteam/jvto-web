34944b8c1bfcde08356b97029d2ae4772fad97ac
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2026-05-08 15:44:57 +0700

    update

--- src/components/website/Home/Hero.tsx
+++ src/components/website/Home/Hero.tsx
@@ -38,13 +38,43 @@ const Hero: React.FC<HeroProps> = ({
           {description}
         </p>
 
-        <div className="flex flex-col items-center gap-4">
-          <Button target="_blank" to="/tours" variant="primary" size="lg">
-            View Private Tours
+        <div className="flex gap-4 justify-center md:hidden">
+          <Button
+            target="_blank"
+            to="/tours"
+            prefetch={false}
+            variant="primary"
+            size="md"
+          >
+            Private Tours
           </Button>
-          <a
-            href="/verify-jvto"
-            className="text-sm text-gray-300 hover:text-white underline underline-offset-4 transition-colors"
+          <Button
+            to="/verify-jvto"
+            prefetch={false}
+            variant="outline"
+            size="md"
+            className="border-white text-white hover:bg-white hover:!text-black"
+          >
+            Verify JVTO
+          </Button>
+        </div>
+        <div className="md:flex gap-4 justify-center hidden">
+          <Button
+            target="_blank"
+            to="/tours"
+            prefetch={false}
+            variant="primary"
+            size="lg"
+          >
+            Private Tours
+          </Button>
+          <Button
+            target="_blank"
+            to="/verify-jvto"
+            prefetch={false}
+            variant="outline"
+            size="lg"
+            className="border-white text-white hover:bg-white hover:!text-black"
           >
             Verify our licenses and credentials
           </a>
